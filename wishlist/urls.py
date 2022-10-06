bb1afb09bb46e24393bd8b23f7944e995cb5b76d
vian1015 <jovian.marvin@ui.ac.id>
2022-10-06 13:25:45 +0700

    Lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_ajax
 from wishlist.views import show_xml
 from wishlist.views import show_xml_by_id 
 from wishlist.views import show_json
@@ -12,6 +13,7 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('json/', show_json, name='show_json'),
--- wishlist/views.py
+++ wishlist/views.py
@@ -23,6 +23,14 @@ def show_wishlist(request):
 
     return render(request, "wishlist.html", context)
 
+def show_wishlist_ajax(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+        'list_item': data_wishlist_item,
+    }
+
+    return render(request, "wishlist_ajax.html", context)
+
 def show_xml(request):
     data = ItemWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
